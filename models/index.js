5de3031582468c4b03da49dcd7f9943661d5e449
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-10-03 19:19:27 -0400

    looped through todo data

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 // Set debugger to see errors for DB
 mongoose.set('debug', true);
-mongoose.connect('mongo://localhost/todo-api');
+mongoose.connect('mongodb://localhost/todo-api');
 
 // Enabling promises to be utilized
 mongoose.Promise = Promise;
--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-alert("Hi from app.js");
+$(document).ready(function() {
+    $.getJSON("api/todos")
+    .then(addTodos) 
+});
+
+function addTodos(todos) {
+    // add todos to the page here
+    todos.forEach(function(todo) {
+        var newTodo = $('<li class="task">' +todo.name + '</li>');
+        $('.list').append(newTodo);
+    })
+}
