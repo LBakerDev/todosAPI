1dbb7da43e305c81fc8870932b51ff7715757648
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-10-05 00:20:06 -0400

    added delete

--- public/app.js
+++ public/app.js
@@ -1,12 +1,58 @@
 $(document).ready(function() {
     $.getJSON("api/todos")
     .then(addTodos) 
+
+    $('#todoInput').keypress(function(event) {
+        if(event.which === 13) {
+            createTodo();
+        }
+    })
+
+    $('.list').on("click",'span', function() {
+        removeTodo($(this).parent());
+    })
 });
 
 function addTodos(todos) {
     // add todos to the page here
     todos.forEach(function(todo) {
-        var newTodo = $('<li class="task">' +todo.name + '</li>');
-        $('.list').append(newTodo);
+       addTodo(todo);
+    })
+}
+
+function createTodo() {
+    // send post request
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos', {name: usrInput })
+    // We get newTodo back from server and then run to append
+    .then(function(newTodo) {
+       $('#todoInput').val('');
+       addTodo(newTodo);
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+}
+
+// Function to add single to do
+function addTodo(todo) {
+    var newTodo = $('<li class="task">' +todo.name + ' <span>X</span></li>');
+    newTodo.data('id', todo._id);
+    if(todo.completed) {
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
+function removeTodo(todo) {
+    var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/' + clickedId;
+    $
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function(data) {
+        todo.remove();
     })
 }
