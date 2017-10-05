2cc1abf91e62a3e2dc110879f3299d8f752a2586
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-10-05 00:42:12 -0400

    complete

--- public/app.js
+++ public/app.js
@@ -8,7 +8,13 @@ $(document).ready(function() {
         }
     })
 
-    $('.list').on("click",'span', function() {
+    $('.list').on('click', 'li', function () {
+        updateTodo($(this));
+    })
+
+    $('.list').on("click",'span', function(e) {
+        // Stops the 'X' click from bubbling to 'li'
+        e.stopPropagation();
         removeTodo($(this).parent());
     })
 });
@@ -38,6 +44,7 @@ function createTodo() {
 function addTodo(todo) {
     var newTodo = $('<li class="task">' +todo.name + ' <span>X</span></li>');
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if(todo.completed) {
         newTodo.addClass("done");
     }
@@ -55,4 +62,23 @@ function removeTodo(todo) {
     .then(function(data) {
         todo.remove();
     })
+}
+
+function updateTodo(todo){
+    var updateUrl = '/api/todos/' + todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone}
+
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
+
+    })
+    .then(function(updatedTodo) {
+        todo.toggleClass("done");
+        todo.data('completed', isDone);
+        
+    })
+
 }
